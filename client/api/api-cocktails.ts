903a3358c04ffe5b53d73b3bb2675578b6168973
anthonymartin36 <anthony.martin36@gmail.com>
2024-05-28 14:21:02 +1200

    changes including api

---
+++ client/api/api-cocktails.ts
@@ -0,0 +1,22 @@
+import request from 'superagent'
+// import { Cocktail } from '../../models/cocktails'
+const rootUrl = import.meta.env.API_URL || 'https://cocktail-z2ed.onrender.com/api/v1/cocktails/'
+
+export async function getAllCocktails(){ 
+    try {
+      const response = await request.get(rootUrl)
+      return response.body
+    } catch (error) {
+      throw console.error('Error fetching missing cocktails', error)
+    }
+  }
+
+  export async function getACocktail(cId: number) {
+    try {
+      const response = await request.get(rootUrl + cId)
+      return response.body
+    } catch (error) {
+        console.error(`Error fetching cocktail with id ${cId}: `, error)
+        throw new Error(`Failed to fetch cocktail with id ${cId}`)
+      }
+  }
