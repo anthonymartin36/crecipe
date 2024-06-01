8faece98739801777d829a4521c2e40f5f0b6ae1
anthonymartin36 <anthony.martin36@gmail.com>
2024-06-01 13:55:44 +1200

    changes to Nav

--- client/components/Cocktail.tsx
+++ client/components/Cocktail.tsx
@@ -1,45 +1,45 @@
 import Footer from './Footer.tsx'
-// import Nav from './Navbar.tsx'
-// import { useParams } from 'react-router-dom'
-// import { useQuery } from '@tanstack/react-query'
-// import { getACocktailApi } from '../api/api-cocktails.ts'
+import Nav from './Navbar.tsx'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getACocktailApi } from '../api/api-cocktails.ts'
 import './Cocktail.css'
 
 function Cocktail() {
-  // const { cId } = useParams()
+  const { cId } = useParams()
 
-  // const {
-  //   data: cocktail,
-  //   isLoading,
-  //   isError,
-  // } = useQuery({
-  //   queryKey: ['cocktail', cId],
-  //   queryFn: () => getACocktailApi(Number(cId)),
-  // })
+  const {
+    data: cocktail,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['cocktail', cId],
+    queryFn: () => getACocktailApi(Number(cId)),
+  })
 
-  // if (isError) {
-  //   return (
-  //     <div className="loading">
-  //       <h1 className="loading-heading">Something's broken!</h1>
-  //     </div>
-  //   )
-  // }
+  if (isError) {
+    return (
+      <div className="loading">
+        <h1 className="loading-heading">Something's broken!</h1>
+      </div>
+    )
+  }
 
-  // if (!cocktail || isLoading) {
-  //   return (
-  //     <div className="loading">
-  //       <h1 className="loading-heading">Just a Sec!!</h1>
-  //     </div>
-  //   )
-  // }
-  console.log("Test URL") //cocktail.id, cocktail.title
+  if (!cocktail || isLoading) {
+    return (
+      <div className="loading">
+        <h1 className="loading-heading">Just a Sec!!</h1>
+      </div>
+    )
+  }
+  console.log("Test URL") 
     return (
       <>
         <Nav />
         <div className='cocktail' id='cocktail'>
         <div className='container' id='container'>
-        <div className='content' key='1'>
-        <p>Cocktail :     </p> </div>
+        <div className='content' key={cocktail.id}>
+        <p>Cocktail :   { cocktail.title } </p> </div>
         </div>
         </div>
         <Footer />
